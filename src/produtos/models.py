ead56ea4d6636b156b071dc12ab79d79fd384621
Marcus Paredes <viniciusparedes@gmail.com>
2022-05-26 09:17:59 -0300

    Create stock control methods on Models

--- src/produtos/models.py
+++ src/produtos/models.py
@@ -8,7 +8,15 @@ class Produto(models.Model):
     quantidade = models.IntegerField()
     situacao = models.SmallIntegerField(choices=SITUACOES_PRODUTO)
 
+    def get_previous_balance(self):
+        try:
+            produto = Produto.objects.get(pk=self.pk)
+            return produto.quantidade
+        except Exception as _:
+            return 0
+
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.quantidade += self.get_previous_balance()
         self.situacao = 0 if self.quantidade < 1 else 1
         super(self).save(force_insert, force_update, using, update_fields)
 
@@ -22,3 +30,12 @@ class Pedido(models.Model):
     solicitante_endereco = models.CharField(max_length=360)
     despachante = models.CharField(max_length=124, null=False, blank=False)
     situacao = models.SmallIntegerField(choices=SITUACOES_PEDIDO)
+
+    @property
+    def has_stock(self):
+        return self.produto.quantidade >= self.quantidade
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not self.has_stock:
+            raise Exception("Este produto não possui o estoque necessário para atender ao pedido.")
+        super(self).save(force_insert, force_update, using, update_fields)
