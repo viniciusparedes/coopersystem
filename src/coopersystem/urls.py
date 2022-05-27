a4897df1a67c4825d532312e3e17ce5fd61865cd
Marcus Paredes <viniciusparedes@gmail.com>
2022-05-26 23:05:20 -0300

    Install Swagger

--- src/coopersystem/urls.py
+++ src/coopersystem/urls.py
@@ -1,23 +1,9 @@
-"""coopersystem URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
+from django.conf.urls import url
 from rest_framework import routers
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
-from produtos.views import (ProdutoViewSet, PedidoViewSet)
+from produtos.views import (ProdutoViewSet, PedidoViewSet, schema_view)
 
 
 router = routers.DefaultRouter()
@@ -26,6 +12,8 @@ router.register(r'produtos', ProdutoViewSet)
 router.register(r'pedidos', PedidoViewSet)
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'^$', schema_view, name='docs'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
